77c4cb083cc6e270fc1f1213503c85b6f935365d
wajih <wajih@DESKTOP-AT0D6LQ>
2024-08-20 14:36:44 +0300

    Add project files.

---
+++ Studentcontroller/Controllers/StudentsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Studentcontroller.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Getall()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        public IActionResult index()
+        {
+            return View();
+        }
+    }
+}
